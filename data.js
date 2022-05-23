0641834159814729a743320b42b75bbb489bbf15
MJ8889 <madhugjadhav@gmail.com>
2022-05-23 10:41:57 +0800

    SR

---
+++ data.js
@@ -0,0 +1,172 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-boat-quay",
+      "name": "Boat Quay",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.42222490820696024,
+        "pitch": 0.024461602949648054,
+        "fov": 1.4209758406923423
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7782418701390927,
+          "pitch": -0.1610066139402182,
+          "rotation": 0,
+          "target": "1-clarke-quay"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6153203084144501,
+          "pitch": -0.1321280328033403,
+          "title": "Bird Sculpture",
+          "text": "Singapore, Bird by Colombian artist Fernando Botero. This huge bronze sculpture is a well-known sight along the Singapore River, located near United Overseas Bank (UOB) Plaza at Raffles Place.&nbsp;<div><br></div><div>Some might think the sculpture resembles a pigeon but it is actually a representation of a dove, which signifies peace and serenity, and also embodies the joy of living and power of optimism.<br></div>"
+        },
+        {
+          "yaw": 1.3703472194320767,
+          "pitch": -0.20293069560514,
+          "title": "<div>Homage To Newton</div>",
+          "text": "Dalí honours and commends Newton for his discovery of the lawof gravity, symbolised by the famed falling apple, represented here by a sphere of metal attached to a string, thus losing its impermanence as well as it’s incapacity for regeneration.<br>"
+        },
+        {
+          "yaw": -2.7471223084332372,
+          "pitch": -0.10589909274631815,
+          "title": "Molly Malone's Irish Pub",
+          "text": "Oldest Irish pub in Singapore<br>"
+        }
+      ]
+    },
+    {
+      "id": "1-clarke-quay",
+      "name": "Clarke Quay",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8297661200464077,
+          "pitch": -0.13919403476437964,
+          "rotation": 0,
+          "target": "2-robertson-quay"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.9104629322293878,
+          "pitch": 0.13348736991674848,
+          "title": "Water Fountain",
+          "text": "Water Fountain during the day and concert/live band during events night."
+        },
+        {
+          "yaw": 2.876478846171617,
+          "pitch": 0.06214179923115282,
+          "title": "International Cuisine",
+          "text": "International cuisine around the world. Hosting of Food festivals"
+        }
+      ]
+    },
+    {
+      "id": "2-robertson-quay",
+      "name": "Robertson Quay",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.129716321357165,
+          "pitch": -0.037677095719569564,
+          "rotation": 0,
+          "target": "0-boat-quay"
+        },
+        {
+          "yaw": 2.176402587380837,
+          "pitch": -0.15806295020373184,
+          "rotation": 0,
+          "target": "0-boat-quay"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.133989402705085,
+          "pitch": -0.035564795846292796,
+          "title": "Residential and Commercial Space",
+          "text": "In the 1880s, the warehouses and wharfs are built in European and Chinese style. In 1990s, the URA (Urban Redevelopment Authority) made this area for hotel, commercial and residential uses.<br>"
+        },
+        {
+          "yaw": -0.4196004253869141,
+          "pitch": -0.03905327115338331,
+          "title": "Dr. J. Murray Robertson,",
+          "text": "This affluent neighbourhood along the Singapore River got its moniker from Dr. J. Murray Robertson, a prominent municipal councillor from Singapore’s old days."
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
